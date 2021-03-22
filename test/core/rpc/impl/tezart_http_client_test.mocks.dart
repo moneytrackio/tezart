9e0685bf729f99100c12834ad079c2e43122bd95
othmane el kesri <othmane@moneytrack.io>
2021-03-22 13:50:05 +0100

    test(core): test TezartHttpClient

---
+++ test/core/rpc/impl/tezart_http_client_test.mocks.dart
@@ -0,0 +1,11 @@
+import 'package:mockito/mockito.dart' as _i1;
+import 'package:dio/src/dio.dart' as _i2;
+
+/// A class which mocks [Dio].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockDio extends _i1.Mock implements _i2.Dio {
+  MockDio() {
+    _i1.throwOnMissingStub(this);
+  }
+}
