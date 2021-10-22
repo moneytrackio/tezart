1537053659e9b9aa131e9f79d7a9036bcffd2935
Guillaume Lavignotte <guillaume@moneytrack.io>
2021-10-22 15:45:46 +0200

    feat(http): retry on http get request fail

--- lib/src/core/rpc/impl/tezart_http_client.dart
+++ lib/src/core/rpc/impl/tezart_http_client.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart' as http_client;
 import 'package:logging/logging.dart';
+import 'package:retry/retry.dart';
 
 import 'tezart_http_error.dart';
 
@@ -30,20 +33,40 @@ class TezartHttpClient {
 
   Future<http_client.Response> get(String path, {Map<String, dynamic>? params}) {
     log.info('request to get from path: $path');
-    return _handleClientError(() => client.get(path, queryParameters: params));
+
+    return _retryOnTezartHttpError(
+      () => _handleClientError(
+        () => client.get(path, queryParameters: params),
+      ),
+    );
   }
 
   Future<http_client.Response<http_client.ResponseBody>> getStream(
     String path, {
     Map<String, dynamic>? params,
   }) {
-    return _handleClientError(() => client.get<http_client.ResponseBody>(
+    return _retryOnTezartHttpError(
+      () => _handleClientError(
+        () => client.get<http_client.ResponseBody>(
           path,
           queryParameters: params,
           options: http_client.Options(
             responseType: http_client.ResponseType.stream,
           ), // set responseType to `stream`
-        ));
+        ),
+      ),
+    );
+  }
+
+  Future<T> _retryOnTezartHttpError<T>(Future<T> Function() func) {
+    final r = RetryOptions(maxAttempts: 3);
+
+    return r.retry<T>(
+      func,
+      retryIf: (e) {
+        return e is TezartHttpError && e.originalException is SocketException;
+      },
+    );
   }
 
   Future<T> _handleClientError<T>(Function func) async {
